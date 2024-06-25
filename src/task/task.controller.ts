330a865efc516d26aa52da87d3ea91c78e1a796a
Yuri Tokio <yuri@sheild.com.br>
2024-06-25 18:11:19 -0300

    criação da rota Find by Id

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,5 @@
 import { TaskService } from './task.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Controller('task') // Aqui estamos definindo nosso endpoint /task
@@ -12,4 +12,12 @@ export class TaskController {
     create(@Body() task: TaskDto){       // Método que irá receber a taskDTO criada 
         this.TaskService.create(task)
     }
+
+    @Get('/:id')        // :id é um parametro
+    findById(@Param('id') id: string) : TaskDto{     
+        // nome do método 
+        // @Param é um 'Route Param' para obter todos os parametros passados na rota
+        // : TaskDto é pra definirmos que o tipo de retorno será TaskDto
+        return this.TaskService.findById(id)
+    }   
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
@@ -11,4 +11,16 @@ export class TaskService {
         this.tasks.push(task)   // inserindo objeto no array (task)
         console.log(this.tasks)
     }
+
+    findById(id: string): TaskDto {
+        const foundTask = this.tasks.filter(t => t.id === id);
+        // isso em cima, é pra saber se no array tasks temos alguem com o mesmo id do parâmetro
+
+        if (foundTask.length){
+            return foundTask[0]
+        }
+
+        throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND)
+    }
+
 }
