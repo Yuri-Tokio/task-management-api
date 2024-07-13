ed71fcf1166f8ecdab228abb4cbd03dc451952c1
Yuri Tokio <yuri@sheild.com.br>
2024-07-13 18:27:36 -0300

    Ajuste erro no auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,47 +1,41 @@
-import { ConfigService } from '@nestjs/config';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
-
 @Injectable()
-export class AuthGuard implements CanActivate { 
-
+export class AuthGuard implements CanActivate {
+  
   private jwtSecret: string;
-
-  constructor(private readonly jwtService: JwtService, private readonly configService: ConfigService) {
-    this.jwtSecret = this.configService.get<string>('JWT_SECRET')
+  
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ) {
+    this.jwtSecret = this.configService.get<string>('JWT_SECRET');
   }
-
-  async canActivate(  // por se tratar de uma promise, usaremos async
-    context: ExecutionContext,
-  ): Promise<boolean> { 
-
-    const request = context.switchToHttp().getRequest();  // ter acesso as requisições do express
-
-    const token = this.extractTokenFromHeader(request)
-
-    if (!token){
-      throw new UnauthorizedException()
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    
+    const request = context.switchToHttp().getRequest();
+    
+    const token = this.extractTokenFromHeader(request);
+    
+    if (!token) {
+      throw new UnauthorizedException();
     }
-
+    
     try {
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        {
-          secret:this.jwtSecret
-        }
-      )
-      request['user'] = payload
-    } catch{
-      throw new UnauthorizedException()
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.jwtSecret,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
     }
-
-    return true // se retornar true é pq temos nosso usuário autenticado
+    
+    return true;
   }
   private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split('')??[];
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
-    // estamos extraindo o token da requisição e verificando se o type é do tipo Bearer. Se for retorna token, do contrário retorna undefined
   }
 }
