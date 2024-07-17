637f909028ab9a4c648029df1689528bf5118889
Yuri Tokio <yuri@sheild.com.br>
2024-07-17 19:05:16 -0300

    Criação das entidades

---
+++ src/db/entities/task.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'task' })
+export class TaskEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+
+    @Column({type: 'varchar'})
+    title: string;
+
+
+    @Column({type: 'varchar'})
+    description: string;
+
+
+    @Column({type: 'varchar'})
+    status: string;
+
+    
+    @Column({type: 'timestamptz', name: 'expiration_date'})
+    expirationDate: Date;
+}
---
+++ src/db/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'user' })
+export class UserEntity {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    
+    @Column({type: 'varchar'})
+    username: string;
+
+    
+    @Column({type: 'varchar', name: 'password_hash'})
+    passwordHash: string;
+}
