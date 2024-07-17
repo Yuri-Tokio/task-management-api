60af6155935de5832cf00993856724b0869239ea
Yuri Tokio <yuri@sheild.com.br>
2024-07-17 16:33:49 -0300

    Banco de dados funcional e conectado com aplicação
    Foi configurado o módulo do banco de dados de forma assíncrona e com inicialização dinâmica.
    
    banco de dados: PostgreSQL
    SGBD: DBeaver
    
    Falta configurar as migrations e as entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true}), TaskModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true}), TaskModule, UsersModule, AuthModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
