4b2d06ba3b7c39c916c50a2d921bf862e7a776e3
Yuri Tokio <yuri@sheild.com.br>
2024-07-17 18:54:51 -0300

    Migrations criadas, configuradas e executadas.
    Nosso banco de dados ja está apto para receber as entidades

---
+++ src/db/typeOrm.migration-config.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+
+config();   // disponibilizando variáveis de ambiente
+
+const configService = new ConfigService()
+
+const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: +configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [],         
+    migrations: [__dirname + '/migrations/*.ts'],   
+    synchronize: false
+}
+
+export default new DataSource(dataSourceOptions)
+
+// a gente consegue executar o typeorm para criar e executar migrations e identificar onde eles estão localizados
