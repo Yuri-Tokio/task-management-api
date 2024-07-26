707fdac42e11c29cb959ec0cf5ec552f34e8ef19
Yuri Tokio <yuri@sheild.com.br>
2024-07-26 00:58:14 -0300

    ajuste task module (forFeature)
    Service nao foi mexida

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntity } from 'src/db/entities/task.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([TaskEntity])],
     controllers: [TaskController],
     providers: [TaskService]
 })
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -4,36 +4,36 @@ import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
-    
-    private tasks: TaskDto[] = [] 
-    
-    create(task: TaskDto){
+
+    private tasks: TaskDto[] = []
+
+    create(task: TaskDto) {
         task.id = uuid()    // nova task com id automático
         task.status = TaskStatusEnum.TO_DO  // nova task com status automático
-        
-        this.tasks.push(task) 
+
+        this.tasks.push(task)
     }
 
-  findById(id: string): TaskDto {
+    findById(id: string): TaskDto {
         const foundTask = this.tasks.filter(t => t.id === id);
 
-        if (foundTask.length){
+        if (foundTask.length) {
             return foundTask[0]
         }
 
         throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND)
     }
 
-    findAll(params: FindAllParamaters): TaskDto[]{
-        
+    findAll(params: FindAllParamaters): TaskDto[] {
+
         return this.tasks.filter(t => {
             let match = true;
 
             if (params.title != undefined && !t.title.includes(params.title)) {
                 match = false
             }
-            
-            if (params.status != undefined && !t.status.includes( params.status)) {
+
+            if (params.status != undefined && !t.status.includes(params.status)) {
                 match = false
             }
 
@@ -41,21 +41,21 @@ export class TaskService {
         })
     }
 
-    update(task: TaskDto){
+    update(task: TaskDto) {
         let taskIndex = this.tasks.findIndex(t => t.id === task.id);
 
         if (taskIndex >= 0) {
             this.tasks[taskIndex] = task
             return;
         }
-        
+
         throw new HttpException(`Task with id ${task.id} not found`, HttpStatus.BAD_REQUEST)
     }
 
-    remove(id: string){
-        let taskIndex = this.tasks.findIndex(t => t.id === id) 
+    remove(id: string) {
+        let taskIndex = this.tasks.findIndex(t => t.id === id)
 
-        if (taskIndex >= 0){
+        if (taskIndex >= 0) {
             this.tasks.slice(taskIndex, 1)
             return
         }
